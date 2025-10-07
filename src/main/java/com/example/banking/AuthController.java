aa268e38643813e5e6fcfb3e64376950bf634d35
Srujana1056 <srujanananduri1056@gmail.com>
2025-10-07 10:42:15 +0530

    Updated url of tomcat

--- src/main/java/com/example/banking/AuthController.java
+++ src/main/java/com/example/banking/AuthController.java
@@ -6,7 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin(origins = "http://localhost:5173")
+@CrossOrigin(origins = "http://localhost:9090")
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
